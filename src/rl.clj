ce5e18300522f0faaea3a242a4310f672af41bfb
Pedro Miller Rabinovitch <pedromr@gmail.com>
2011-09-18 18:39:27 +0100

    Functional rendering

--- src/rl.clj
+++ src/rl.clj
@@ -7,27 +7,66 @@
 
 (println "Hello!" )
 
+(defn handle-input [#^KeyEvent event]
+  (println (.getKeyCode event) (.getKeyChar event)))
+
+(defn render-row-as-string [mapView row cols]
+  (loop [line ""
+         col 0
+         cols cols]
+    (if (> cols 0)
+      (recur (str line ".") (inc col) (dec cols))
+      (str line "\n"))))
+
+(defn render-map-as-string [mapView rows cols]
+  (loop [text ""
+         row 0
+         rows rows]
+    (if (> rows 0)
+      (recur (str text (render-row-as-string mapView row cols)) (inc row) (dec rows))
+      text)))
+
+(defn display-grid [#^TextArea textArea mapView]
+  (.setText textArea (.toString (render-map-as-string mapView (.getRows textArea) (.getColumns textArea)))))
+;  (let [view ""]
+;    (dotimes [y (.getRows textArea)]
+;      (dotimes [x (.getColumns textArea)]
+;        (.appendText textArea "."))
+;      (.appendText textArea "\n"))
+;    (.setText textArea view)))
+
+(defn map-view [width height]
+
+  )
+
 (defn input-listener []
   (proxy [ActionListener KeyListener] []
     (actionPerformed [e])
-    (keyPressed [e] (println e)) ;handle-input
+    (keyPressed [e] (handle-input e))
     (keyReleased [e])
     (keyTyped [e])))
 
-
 (let [frame (JFrame. "RL")
-      textArea (TextArea. "Hubba!")]
+      textArea (TextArea. "Hubba!")
+      view-rows 30
+      view-cols 24]
   (doto frame
     (.setSize 640 480)
     (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
     (.setResizable false)
     (.add textArea)
+    (.addKeyListener (input-listener))
+    (.requestFocus)
     (.setVisible true))
   (doto textArea
     (.setFont (Font. "Monospaced" (Font/PLAIN) 11))
-    (.setColumns 80)
-    (.setRows 24)
-    (.addKeyListener (input-listener))
+    (.setColumns view-rows)
+    (.setRows view-cols)
+    (.setFocusable false)
     (.setForeground (Color/GREEN))
     (.setBackground (Color/BLACK))
-    ))
+    )
+
+  (display-grid textArea (map-view 100 100))
+
+  )
