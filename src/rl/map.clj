1e898a0ba9e34e4dde830408402fc3154bd23de9
Pedro Miller Rabinovitch <pedromr@gmail.com>
2011-09-21 20:49:45 +0100

    Walls can be added! Walkability checked!

--- src/rl/map.clj
+++ src/rl/map.clj
@@ -6,8 +6,15 @@
   (in-bounds? [this x y])
   (walkable? [this x y]))
 (defprotocol MapEdit
-  (with-wall-at [this x y]))
-(deftype Map
+  (with-wall-at [this x y])
+  (add-position-to-set [this set-index position]))
+
+;(defstruct map-struct :width :height :walls)
+;
+;(defn new-map [width height]
+;  (create-struct width height {}))
+
+(defrecord Map
   [width height]
   MapView
   (in-bounds? [this x y] (and
@@ -15,11 +22,27 @@
                            (>= y 0)
                            (>= x 0)
                            (< x width)))
-  (walkable? [this x y] (in-bounds? this x y))
+  (walkable? [this x y] (and
+                          (in-bounds? this x y)
+                          (nil?
+                            (get
+                              (get this :walls)
+                              [x y]))))
   MapEdit
-  (with-wall-at [this x y] this))
-
-(let [map (with-wall-at (Map. 50 50) 20 20)]
+  (with-wall-at [this x y]
+    (add-position-to-set this :walls [x y]))
+  (add-position-to-set [this set-index position]
+    (let [new-map (assoc
+      this
+      set-index
+      (conj
+        (or (get this set-index) #{})
+        position)
+    )]
+      (prn new-map)
+      new-map))
+  )
+(let [map (with-wall-at (with-wall-at (Map. 50 50) 20 20) 21 20)]
   (deftest testBounds
     (is (in-bounds? map 30 30))
     (is (not (in-bounds? map 50 30)))
@@ -28,8 +51,10 @@
     (is (not (in-bounds? map 30 -1)))
     )
   (deftest testWalkable
-    (is (walkable? map 10 10)))
+    (is (walkable? map 10 10))
+    (is (walkable? map 21 21))
     (is (not (walkable? map 100 10)))
     (is (not (walkable? map 20 20)))
   )
+)
 
